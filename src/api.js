9cf6e1ff13333918b27ee71600c3a4373ad9b1e8
ViktoriiaYakymovych <118008244+ViktoriiaYakymovych@users.noreply.github.com>
2023-11-11 16:11:51 +0200

    added fetches

--- src/api.js
+++ src/api.js
@@ -1,25 +1,70 @@
 import axios from 'axios';
 
 axios.defaults.baseURL = 'https://api.themoviedb.org/';
-// const API_KEY = '48b2bba5f96af80717b061a99685';
+const API_KEY = '48b2bba5f96af80717b061a99685cb65';
 
-// const OPTIONS = {
-//   params: {
-//     language: 'en-US',
-//   },
-//   headers: {
-//     accept: 'application/json',
-//     Authorization: `Bearer ${API_KEY}`,
-//   },
-// };
+const fetchTrendsMovies = async () => {
+  try {
+    const { data } = await axios.get(
+      `/3/trending/all/day?language=en-US&api_key=${API_KEY}`
+    );
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
+};
 
-// const fetchTrendsMovies = async () => {
-//   try {
-//     const response = await axios.get('/3/trending/movie/day', OPTIONS);
-//     console.log(response);
-//   } catch (err) {
-//     console.log(err);
-//   }
-// };
+const fetchSearchMovie = async query => {
+  try {
+    const { data } = await axios.get(
+      `/3/search/movie?query=${query}&include_adult=false&language=en-US&page=1&api_key=${API_KEY}`
+    );
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
+};
 
-// export default fetchTrendsMovies;
+const fetchMovieDetails = async movieId => {
+  try {
+    const { data } = await axios.get(
+      `/3/movie/${movieId}?language=en-US&api_key=${API_KEY}`
+    );
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const fetchMovieActors = async movieId => {
+  try {
+    const { data } = await axios.get(
+      `/3/movie/${movieId}/credits?language=en-US&api_key=${API_KEY}`
+    );
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const fetchMovieReviews = async movieId => {
+  try {
+    const { data } = await axios.get(
+      `/3/movie/${movieId}/reviews?language=en-US&page=1&api_key=${API_KEY}`
+    );
+    console.log(data);
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const api = {
+  fetchTrendsMovies,
+  fetchSearchMovie,
+  fetchMovieDetails,
+  fetchMovieActors,
+  fetchMovieReviews,
+};
+
+export default api;
