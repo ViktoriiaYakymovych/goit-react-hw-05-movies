827efa0c91cc81871b99ff53983f5416f3bb3f35
ViktoriiaYakymovych <118008244+ViktoriiaYakymovych@users.noreply.github.com>
2023-11-16 13:13:57 +0200

    a

--- src/pages/Movies/Movies.jsx
+++ src/pages/Movies/Movies.jsx
@@ -4,22 +4,24 @@ import api from 'api';
 import SearchMovieForm from 'components/SearchMovieForm/SearchMovieForm';
 import MoviesList from 'components/MoviesList/MoviesList';
 import Loader from 'components/Loader/Loader';
+import { useSearchParams } from 'react-router-dom';
 
 const Movies = () => {
   const [films, setFilms] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
-  const [filmQuery, setFilmQuery] = useState('');
+  const [searchParams, setSearchParams] = useSearchParams();
+  const value = searchParams.get('query') ?? '';
 
   useEffect(() => {
-    if (filmQuery === '') {
+    if (value === '') {
       return;
     }
     const controller = new AbortController();
-    const fetchMovies = async q => {
+    const fetchMovies = async () => {
       try {
         setLoading(true);
-        const { results } = await api.fetchSearchMovie(q, controller);
+        const { results } = await api.fetchSearchMovie(value, controller);
         setFilms(results);
       } catch (error) {
         if (error.code !== 'ERR_CANCELED') {
@@ -30,21 +32,21 @@ const Movies = () => {
         setLoading(false);
       }
     };
-     fetchMovies(filmQuery);
+    fetchMovies();
     return () => {
       controller.abort();
     };
-  }, [filmQuery]);
+  }, [value]);
 
   const onSubmit = e => {
     e.preventDefault();
-    const filmQuery = e.target.elements.film.value.trim();
-    if (filmQuery === '') {
+    const newQuery = e.target.elements.film.value.trim();
+    if (newQuery === '') {
       return toast.error(
         `Sorry, we can't submit empty request. Please, write what You want to search.`
       );
     }
-    setFilmQuery(filmQuery);
+    setSearchParams({ query: newQuery });
     setFilms([]);
     e.target.reset();
   };
@@ -52,7 +54,7 @@ const Movies = () => {
     <>
       <SearchMovieForm onSubmit={onSubmit} />
       {films.length > 0 && <MoviesList films={films} />}
-      {loading && <Loader loading={loading}/>}
+      {loading && <Loader loading={loading} />}
       {error && <p>Sorry, something went wrong. Please, try to update page.</p>}
     </>
   );
