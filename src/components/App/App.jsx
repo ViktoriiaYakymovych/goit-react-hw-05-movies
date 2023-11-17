5050b0ff73a5ca22af66c67aeb8524596a07a090
ViktoriiaYakymovych <118008244+ViktoriiaYakymovych@users.noreply.github.com>
2023-11-17 14:33:58 +0200

    a

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,5 @@
 import { lazy } from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import AppLayout from 'AppLayout';
 
 const Home = lazy(() => import('pages/Home/Home'));
@@ -7,7 +7,6 @@ const Movies = lazy(() => import('pages/Movies/Movies'));
 const MovieDetails = lazy(() => import('pages/MovieDetails/MovieDetails'));
 const Cast = lazy(() => import('components/Cast/Cast'));
 const Reviews = lazy(() => import('components/Reviews/Reviews'));
-const NotFound = lazy(() => import('components/NotFound/NotFound'));
 
 export const App = () => {
   return (
@@ -20,7 +19,7 @@ export const App = () => {
           <Route path="reviews" element={<Reviews />} />
         </Route>
       </Route>
-      <Route path="*" element={<NotFound />} />
+      <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   );
 };
--- src/components/NotFound/NotFound.jsx
+++
@@ -1,9 +0,0 @@
-const NotFound = () => {
-  return (
-    <p>
-      <span>{`Ooops... :( `}</span>Sorry, we didn't find any information.
-    </p>
-  );
-};
-
-export default NotFound;
